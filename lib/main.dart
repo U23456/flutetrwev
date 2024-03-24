c48891eab75964fa57b545da8df92cfe293464af
usman <usmanhussainsiddiqui1111@gmail.com>
2024-03-24 14:54:52 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WebView Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: WebViewExample(),
+    );
+  }
+}
+
+class WebViewExample extends StatefulWidget {
+  @override
+  _WebViewExampleState createState() => _WebViewExampleState();
+}
+
+class _WebViewExampleState extends State<WebViewExample> {
+  @override
+  void initState() {
+    super.initState();
+    // Enable hybrid composition for Android WebView
+    WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WebView Demo'),
+      ),
+      body: WebView(
+        initialUrl: 'https://eliestfilm.com/', // Replace with your URL
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
